0e2eb85be65fda35fabf9a974ea9ed0c27bee1a5
shahariarikbal <shahariar.ikbal86@gmail.com>
2025-06-21 00:40:13 +0600

    feat:Show register user list

--- src/index.php
+++ src/index.php
@@ -73,6 +73,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){
   <body>
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="index.php">
+                <a href="register-list.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline float-right mb-5">
+                    User List
+                </a>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
                     Name
@@ -126,6 +129,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="submit">
                     Sign Up
                 </button>
+                <a href="login.php" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                    Already have an account?
+                </a>
             </div>
         </form>
     </div>
